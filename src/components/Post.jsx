04f9f85e013abf56946a7e659f04244404f25542
allentran2606 <giahaot2606@gmail.com>
2024-07-24 10:58:12 +0700

    Feature: Add getFeedPosts function at HomePage.jsx

---
+++ src/components/Post.jsx
@@ -0,0 +1,129 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { Avatar, Box, Flex, Image, Text } from "@chakra-ui/react";
+import { BsThreeDots } from "react-icons/bs";
+
+import ActionButtons from "./ActionButtons";
+import useShowToast from "../hooks/useShowToast";
+
+function Post({ post, postedBy: userId }) {
+  const [liked, setLiked] = useState(false);
+  const [user, setUser] = useState(null);
+
+  const showToast = useShowToast();
+
+  // *********
+  // Optimization: Use aggregation stage to get user's profile while retrieving post
+  // Reference: https://chatgpt.com/c/198134f5-81a3-4b2c-bdfd-b1580af0f656
+  // *********
+
+  // Fetch user
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const res = await fetch("/api/users/profile/" + userId);
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setUser(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+        setUser(null);
+      }
+    };
+
+    getUser();
+  }, [userId, showToast]);
+
+  if (!user) return null;
+
+  return (
+    <Link to={"/markzuckerberg/post/1"}>
+      <Flex gap={3} mb={4} py={5}>
+        {/* Avatars */}
+        <Flex flexDirection={"column"} alignItems={"center"}>
+          <Avatar size={"md"} name={user?.name} src={user?.profilePicture} />
+          <Box w="1px" h={"full"} bg={"gray.light"} my={2}></Box>
+          <Box position={"relative"} w={"full"}>
+            <Avatar
+              size={"xs"}
+              name="John Doe"
+              src="https://bit.ly/dan-abramov"
+              position={"absolute"}
+              top={"0px"}
+              left={"15px"}
+              padding={"2px"}
+            />
+            <Avatar
+              size={"xs"}
+              name="John Doe"
+              src="https://bit.ly/tioluwani-kolawole"
+              position={"absolute"}
+              bottom={"0px"}
+              right={"-5px"}
+              padding={"2px"}
+            />
+            <Avatar
+              size={"xs"}
+              name="John Doe"
+              src="https://bit.ly/prosper-baba"
+              position={"absolute"}
+              bottom={"0px"}
+              left={"4px"}
+              padding={"2px"}
+            />
+          </Box>
+        </Flex>
+
+        <Flex flex={1} flexDirection={"column"} gap={2}>
+          {/* Username + Post's description */}
+          <Flex justifyContent={"space-between"} w={"full"}>
+            <Flex w={"full"} alignItems={"center"}>
+              <Text fontSize={"sm"} fontWeight={"bold"}>
+                {user?.username}
+              </Text>
+              <Image src="/verified.png" w={4} h={4} ml={1} />
+            </Flex>
+
+            <Flex gap={4} alignItems={"center"}>
+              <Text fontStyle={"sm"} color={"gray.light"}>
+                1d
+              </Text>
+              <BsThreeDots />
+            </Flex>
+          </Flex>
+
+          <Text fontSize={"sm"}>{post.text}</Text>
+          {post.img && (
+            <Box
+              borderRadius={6}
+              overflow={"hidden"}
+              border={"1px solid"}
+              borderColor={"gray.light"}>
+              <Image src={post.img} w={"full"} />
+            </Box>
+          )}
+
+          {/* Reaction icons */}
+          <Flex gap={3} my={1} className="reactions">
+            <ActionButtons liked={liked} setLiked={setLiked} />
+          </Flex>
+
+          <Flex gap={2} alignItems={"center"}>
+            <Text color={"gray.light"} fontSize={"sm"}>
+              {post.replies.length} replies
+            </Text>
+            <Box w={0.5} h={0.5} borderRadius={"full"} bg={"gray.light"}></Box>
+            <Text color={"gray.light"} fontSize={"sm"}>
+              {post.likes.length} likes
+            </Text>
+          </Flex>
+        </Flex>
+      </Flex>
+    </Link>
+  );
+}
+
+export default Post;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,55 @@
-import { Button, Flex } from "@chakra-ui/react";
-import { Link } from "react-router-dom";
+import { Flex, Spinner } from "@chakra-ui/react";
+
+import { useEffect, useState } from "react";
+
+import useShowToast from "../hooks/useShowToast";
+import Post from "../components/Post";
 
 function HomePage() {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const showToast = useShowToast();
+
+  useEffect(() => {
+    const getFeedPosts = async () => {
+      setLoading(true);
+      setPosts([]);
+
+      try {
+        const res = await fetch("/api/posts/feed");
+        const data = await res.json();
+        if (data.error) {
+          showToast("Error", data.error, "error");
+          return;
+        }
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    getFeedPosts();
+  }, [showToast, setPosts]);
+
   return (
-    <Link to={"/markzuckerberg"}>
-      <Flex w={"full"} justifyContent={"center"}>
-        <Button mx={"auto"}>Visit Profile Page</Button>
-      </Flex>
-    </Link>
+    <>
+      {!loading && posts.length === 0 && (
+        <h1>Follow some users to see posts</h1>
+      )}
+
+      {loading && (
+        <Flex justify={"center"}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
+    </>
   );
 }
 
